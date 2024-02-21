9cbd36bb831a2e8a8bfd545e8d4cb4e5f494b047
Yahya Mouiguina <ymouigui@e3r1p18.1337.ma>
2024-02-21 10:08:51 +0100

    hh

--- minitalk/client.c
+++ minitalk/client.c
@@ -2,12 +2,12 @@
 #include <signal.h>
 
 
-void send_bits(int pid, char c)
+void send_bits(int pid, unsigned char c)
 {
     int i = 0;
     while(i < 8)
     {
-        if((c & 128) == 128)
+        if(c & 1)
         {
             kill(pid , SIGUSR1);
             printf("1 is sent");
@@ -17,13 +17,47 @@ void send_bits(int pid, char c)
             kill(pid , SIGUSR2);
             printf("0 is sent");
         }
-        c <<=1;
+        c = c >> 1;
+        i++;
+        usleep(1000);
+    }
+}
+void send_len(int pid, long c)
+{
+    int i = 0;
+    while(i < 64)
+    {
+        if(c & 1)
+        {
+            kill(pid , SIGUSR1);
+            printf("1 is sent");
+        }
+        else
+        {
+            kill(pid , SIGUSR2);
+            printf("0 is sent");
+        }
+        c = c >> 1;
         i++;
         usleep(1000);
     }
 }
 
-int main()
+int main(int argc ,char *argv[])
 {
-    
+    long len;
+    int pid;
+    int i;
+
+    pid = atoi(argv[1]);
+    len = ft_strlen(argv[2]);
+    send_len(pid, len);
+    printf("lengh of the string is sent");
+    while(i < len)
+    {
+        send_bits(pid, argv[2][i]);
+        printf("Charactere '%c' is sent", argv[2][i]);
+        i++;
+    }
+    printf("all sent");
 }
